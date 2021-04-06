d3bd931e02a9f337b481fda2808d6c3248191bef
Ubuntu <ubuntu@ip-172-31-13-204.ec2.internal>
2021-04-06 19:35:04 +0000

    Shell  Scipt Change

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #bin/bash
 
-java -jar /var/lib/jenkins/workspace/JenkinsDev/build/libs/JenkinsDev-0.0.1-SNAPSHOT.jar
+/usr/bin/java -jar /var/lib/jenkins/workspace/JenkinsDev/build/libs/JenkinsDev-0.0.1-SNAPSHOT.jar
