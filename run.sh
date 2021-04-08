47333b90f3a86ab63e07b140b65db231beca5993
Ubuntu <ubuntu@ip-172-31-13-204.ec2.internal>
2021-04-08 16:15:55 +0000

    Shell Script Modified

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #bin/bash
 sudo fuser -k 8081/tcp
-nohup/usr/bin/java -jar /home/ubuntu/JenkinsDev-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out &
+nohup/usr/bin/java -jar /home/ubuntu/build/libs/JenkinsDev-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out &
 sleep 10
 exit 0
 
